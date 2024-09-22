3d050baaafebd691b74e0989f5db7581ea208c9b
maolipeng <dosmlp@126.com>
2024-09-23 05:04:55 +0800

    添加依赖包

--- app_config.h
+++ app_config.h
@@ -1,10 +1,19 @@
 #pragma once
 
-#include <cstdint>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QFile>
+#include <openssl/curve25519.h>
+extern "C" {
+#include <mbedtls/base64.h>
+}
 #include "XLog.h"
+
+using namespace nlohmann;
 
 class AppConfig
 {
@@ -35,6 +44,47 @@ public:
         pub_key = QByteArray::fromBase64(pub.toLatin1());
 
         server_port = obj_root.value("server_port").toInt(17799);
+    }
+    void parse2(const std::string& file)
+    {
+        std::ifstream cfg_file(file);
+        json root = json::parse(cfg_file,nullptr,false);
+        if (root.is_discarded() || root.empty()) {
+            SERROR("cfg error!");
+            return;
+        }
+        auto private_key = root.value("private_key","");
+        std::string pub_key = root.value("public_key","");
+        size_t outlen = 0;
+
+        mbedtls_base64_decode(AppConfig::private_key, 64, &outlen, (const uint8_t*)private_key.data(), private_key.length());
+        mbedtls_base64_decode(AppConfig::public_key, 32, &outlen, (const uint8_t*)pub_key.data(), pub_key.length());
+    }
+    //生成ed25519签名密钥对
+    static void ed25519()
+    {
+        uint8_t public_key[32] = {0};
+        uint8_t private_key[64] = {0};
+        ED25519_keypair(public_key, private_key);
+
+        uint8_t out_base64[128] = {0};
+        size_t olen = 0;
+        mbedtls_base64_encode(out_base64, 128, &olen, public_key, 32);
+        std::cout << "----------public_key----------\n";
+        for (int i = 0;i < 32;++i) {
+            std::cout << std::hex << (uint32_t)(public_key[i]);
+        }
+        std::cout << "\n";
+        std::cout << out_base64 << "\n";
+
+        memset(out_base64, 0, 128);
+        mbedtls_base64_encode(out_base64, 128, &olen, private_key, 64);
+        std::cout << "----------private_key----------\n";
+        for (int i = 0;i < 64;++i) {
+            std::cout << std::hex << (uint32_t)(private_key[i]);
+        }
+        std::cout << "\n";
+        std::cout << out_base64 << "\n";
     }
     QByteArray pub_key;
     QByteArray pri_key;
--- main.cpp
+++ main.cpp
@@ -1,64 +1,20 @@
-#include <cstddef>
-#include <cstdint>
-#include <cstring>
-#include <iostream>
-#include <fstream>
+#include <CLI/CLI.hpp>
 #include "app_config.h"
-#include "mbedtls.hpp"
-#include <string>
-#include "json.hpp"
-#include "app_config.h"
-extern "C" {
-#include <mbedtls/base64.h>
-}
-#include <openssl/curve25519.h>
-#include <openssl/base64.h>
 #include "chatclient.h"
 #include "chatserver.h"
 #include "xlog.h"
 
-using namespace nlohmann;
 
-//生成ed25519签名密钥对
-void ed25519()
+
+
+int main(int argc, char** argv)
 {
-    uint8_t out_public_key[32] = {0};
-    uint8_t out_private_key[64] = {0};
-    ED25519_keypair(out_public_key, out_private_key);
+    CLI::App app{"enchat "};
+    argv = app.ensure_utf8(argv);
 
-    uint8_t out_base64[128] = {0};
-    EVP_EncodeBlock(out_base64, out_public_key, 32);
-    for (int i = 0;i < 32;++i) {
-        std::cout << std::hex << (uint32_t)(out_public_key[i]);
-    }
-    std::cout << "\n";
-    std::cout << out_base64 << "\n";
 
-    memset(out_base64, 0, 128);
-    EVP_EncodeBlock(out_base64, out_private_key, 64);
-    for (int i = 0;i < 64;++i) {
-        std::cout << std::hex << (uint32_t)(out_private_key[i]);
-    }
-    std::cout << "\n";
-    std::cout << out_base64 << "\n";
-}
-
-int main(int, char**)
-{
     XLogMgr::get()->InitLog("./","enchat","enchat");
-    std::ifstream cfg_file("cfg.json");
-    json root = json::parse(cfg_file,nullptr,false);
-    if (root.is_discarded() || root.empty()) {
-        SERROR("cfg error!");
-        return 0;
-    }
-    auto private_key = root.value("private_key","");
-    std::string pub_key = root.value("public_key","");
-    size_t outlen = 0;
 
-    mbedtls_base64_decode(AppConfig::private_key, 64, &outlen, (const uint8_t*)private_key.data(), private_key.length());
-    mbedtls_base64_decode(AppConfig::public_key, 32, &outlen, (const uint8_t*)pub_key.data(), pub_key.length());
-
-
+    AppConfig::ed25519();
     return 0;
 }
